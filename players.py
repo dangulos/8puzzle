9bf26db1c3772534866718f661544022a0265e32
Daniel Angulo <dangulos@unal.edu.co>
2020-07-05 20:45:21 -0500

    Added solution checker

--- players.py
+++ players.py
@@ -5,14 +5,35 @@ from queue import PriorityQueue
 from tree import Tree
 
 def main():
-    #T = Tree(np.array([[5,2,8],[4,1,7],[0,3,6]]))
     maxDepth = 10
     maxIterations = 10
-    T = Tree(np.array([[4,1,0],[2,6,3],[7,5,8]]))
+    goal = np.array([[1,2,3],[4,5,6],[7,8,0]])
+    l = list(range(9))
+    random.shuffle(l)
+    initState = np.array([[l[0],l[1],l[2]],[l[3],l[4],l[5]],[l[6],l[7],l[8]]])
+    #initState = np.array([[4,1,0],[2,6,3],[7,5,8]])
+    #initState = np.array([[5,2,8],[4,1,7],[0,3,6]])
+    #initState = np.array([[1,2,3],[4,5,6],[8,7,0]])
+
+    print("Initial State:\n", initState)
+
+    flatState = initState.flatten()
+    inv_count = 0
+    print(flatState)
+    for i in range(0,8):
+        for j in range(i+1,9):
+            if flatState[i] and flatState[j] and flatState[i] > flatState[j]:
+                inv_count += 1; 
+    print(inv_count)
+    if inv_count%2 == 1:
+        print("Not solvable")
+        return 0
+    T = Tree(initState, maxNodes=100000)
 
     t1 = time.process_time()
     T.solveAStar()
     t2 = time.process_time()
+    print("====================================")
     print("A*")
     if T.goalNode is None:
         print("Did not find goal node")
@@ -64,10 +85,7 @@ def main():
     print("Total: ",totali,"Created nodes")
     print("MaxLevel: ",maxIndex)
     print("Took", t2-t1,"seconds to create the tree")
-    #l = list(range(9))
-    #random.shuffle(l)
-    #print(l)
-    #T = Tree(np.array([[l[0],l[1],l[2]],[l[3],l[4],l[5]],[l[6],l[7],l[8]]]))
+    
 
 if __name__ == "__main__":
     main()
